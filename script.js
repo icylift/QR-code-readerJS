c7d41978450678f216be90f5d90a4c3cf81e081f
Aaron Williams <polarizedcuriosity@gmail.com>
2024-02-20 13:56:09 -0800

    added the css for the textarea and the btns, also started to implement some js

---
+++ script.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelector("form");
+const fileInp = document.querySelector("input");
+const infoText = document.querySelector("p");
+const closeBtn = document.querySelector(".close");
+const copyBtn = document.querySelector(".copy");
